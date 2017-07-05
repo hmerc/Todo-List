e08870a63fdf450f7c37e1b3efcc30546aa58f9c
Heather Mercieca <heather.mercieca@tipico.com>
2017-07-05 10:00:01 +0200

    Created Board container, board css

---
+++ src/containers/board.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Board extends Component {
+	render() {
+    	return (
+      		<div className="board">
+      			<div className="board__column"></div>
+      			<div className="board__column"></div>
+      			<div className="board__column"></div>
+      		</div>
+    	)
+  	}
+
+};
