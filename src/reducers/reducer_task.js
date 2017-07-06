87f1958aaf591cd07db51a20edc97ee092413cb3
Heather Mercieca <heather.mercieca@tipico.com>
2017-07-06 23:25:45 +0200

    Set my state tree to better handle requirements

--- src/reducers/reducer_task.js
+++ src/reducers/reducer_task.js
@@ -1,41 +1,82 @@
-import { ADD_TASK, GET_TASKS_TODO, MOVE_PROGRESS, MOVE_DONE, DELETE_TASK } from '../actions/index';
+import { ADD_TASK, GET_TASKS_TODO, MOVE_PROGRESS, MOVE_DONE, DELETE_TASK, MOVE_TODO } from '../actions/index';
 
 const initialState = {
+  hoursTODO: 0,
+  hoursPROGRESS:0,
+  message:'',
   tasks: []
 }
 
-export default function(state = [], action) {
+export default function(state = initialState, action) {
 	switch(action.type) {
 		case ADD_TASK:
-			return [{title:action.title,hours:action.hours,state:'TODO'}, ...state];
+
+				return Object.assign({}, state, {
+        			tasks: [
+          				...state.tasks,
+         				{
+            				title:action.title,
+            				hours:action.hours,
+            				state:'TODO'
+          				}
+        			],
+        			hoursTODO: parseInt(state.hoursTODO, 10) + parseInt(action.hours, 10)
+      			});
+			
 		
 		case GET_TASKS_TODO:
 			return state
 
 		case MOVE_DONE:
-			return state.map(task => {
-				if(task === action.task) {
-					return Object.assign({}, task, {
-          				state: 'DONE'
-        			});
-				}
-				return task;
-			});
+			return Object.assign({}, state, {
+    			tasks: state.tasks.map((task) => {
+      				if (task === action.task) {
+        				return Object.assign({}, task, {
+          					state: 'DONE'
+        				});
+      				}
+      				
+      				return task;
+   				 })
+   			});
 
 		case MOVE_PROGRESS:
-			return state.map(task => {
-				if(task === action.task) {
-					return Object.assign({}, task, {
-          				state: 'PROGRESS'
-        			});
-				}
-				return task;
-			});
+			return Object.assign({}, state, {
+    			tasks: state.tasks.map((task) => {
+      				if (task === action.task) {
+        				return Object.assign({}, task, {
+          					state: 'PROGRESS'
+        				});
+      				}
+      				
+      				return task;
+   				 }),
+    			hoursPROGRESS: parseInt(state.hoursPROGRESS, 10) + parseInt(action.task.hours, 10)
+  			
+  			});
+
+		case MOVE_TODO:
+			return Object.assign({}, state, {
+    			tasks: state.tasks.map((task) => {
+      				if (task === action.task) {
+        				return Object.assign({}, task, {
+          					state: 'TODO'
+        				});
+      				}
+      				
+      				return task;
+   				 })
+  			});
 		
 		case DELETE_TASK:
-			return state.filter(function (task) {
-				return task !== action.task;
-			});
+			return Object.assign({}, state, {
+    			tasks: state.tasks.filter((task) => {
+      				if (task === action.task) {
+        				return task !== action.task;
+      				}
+      				return task;
+   				 })
+  			});
 		default:
 			return state;
 	}
