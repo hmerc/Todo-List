eceee4bb1d2a4c01cceb17d1e454db356de85cf9
Heather Mercieca <heather.mercieca@tipico.com>
2017-07-10 22:00:27 +0200

    Validated Edit Task Form

--- src/components/edit_task.js
+++ src/components/edit_task.js
@@ -22,12 +22,21 @@ class EditTask extends Component {
       			<form onSubmit={handleSubmit(this.onFormSubmit.bind(this))} className="task--new">
       				<div className="task__title">
       					<input type="text" ref="newField" {...title} />
+                <div className="task__help">
+                  {title.touched ? title.error : ''}
+                </div>
 					    </div>
               <div className="task__description">
                 <textarea type="text" className="form-control" {...description} />
+                <div className="task__help">
+                  {description.touched ? description.error : ''}
+                </div>
               </div>
 					    <div className="task__hours">
 						    <input type="number" className="task_hours" {...hours} />
+                <div className="task__help">
+                  {hours.touched ? hours.error : ''}
+                </div>
       				</div>
               <div className="task__buttons">
       				  <button type="submit" className="task--new__add">Save</button>
@@ -49,6 +58,26 @@ function mapStateToProps(state, props) {
 	}
 }
 
+function validate(values) {
+  const errors = {};
+  if(!values.title) {
+    errors.title = 'Enter a title';
+  }
+  if(!values.hours) {
+    errors.hours = 'Enter hours';
+  }
+  if(!values.description) {
+    errors.description = 'Enter a description';
+  }
+
+  if(values.hours > 8){
+    errors.hours = 'Task Time cannot be more than 8 hours';
+  }
+
+  return errors;
+}
+
+
 
 function mapDispatchToProps(dispatch) {
 	return bindActionCreators({ editTask, editSaveTask }, dispatch);
@@ -57,5 +86,6 @@ function mapDispatchToProps(dispatch) {
 export default reduxForm({
 form: 'EditForm',
 fields: ['title','hours','description'],
+validate,
 enableReinitialize : true
 }, mapStateToProps, mapDispatchToProps)(EditTask);
